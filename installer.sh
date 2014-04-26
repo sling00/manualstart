b564531b00f5eae1e2aa0d4da33bcb0fb3142c60
Sling00 <sling00@gmail.com>
2014-04-26 17:40:43 -0500

    Added basic installer script for pimp

---
+++ installer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Installer script for manualstart.
+#Only works on pimp for now -> http://www.getpimp.org
+#Universal version of script will come eventually.
+
+echo "Installing into /opt/pimp"
+if [ -e /opt/pimp/manualstart ];
+	then 
+	mv /opt/pimp/manualstart /opt/pimp/manualstart.bak
+	else
+fi
+	cp ./manualstart /opt/pimp/manualstart
+if [ -e /opt/pimp/manualstart.conf ];
+	then
+	echo "Backing up your old manualstart.conf to /opt/pimp/manualstart.conf.bak"
+	mv /opt/pimp/manualstart.conf /opt/pimp/manualstart.bak
+	else
+fi
+cp ./manualstart.conf /opt/pimp/manualstart.conf
+echo "Finished, you can now start the script by typing "manualstart".
